91a8006a8ae422abe3e4b51fc9214a4e6188d8d7
jc259420 <jesse.hermon@my.jcu.edu.au>
2015-03-16 11:01:26 +1000

    Prac 3 working

---
+++ src/Patient.java
@@ -0,0 +1,29 @@
+/**
+ * Created by jc259420 on 16/03/15.
+ */
+public class Patient {
+    private String name;
+    private int age;
+    private double height, weight;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setDetails(double height,double weight){
+        this.height = height;
+        this.weight = weight;
+    }
+    public double getBMI(){
+        return (weight / (height * height));
+    }
+}
