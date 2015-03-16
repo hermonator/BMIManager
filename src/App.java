ef2435a83959be34411fbc15f8f6435bcbb5b946
jc259420 <jesse.hermon@my.jcu.edu.au>
2015-03-16 11:20:27 +1000

    Prac 3 working

--- src/App.java
+++ src/App.java
@@ -9,48 +9,62 @@ public class App {
     public static void main(String[] args) {
         Patient patient = new Patient();
         System.out.println("BMI Manager");
-        running: while (true) {
 
-            System.out.println("Please select from the " +
-                    "following menu options:\n" +
-                    "\t1. Add new patient\n" +
-                    "\t2. View patient\n" +
-                    "\t3. Exit\n");
-            System.out.print("Enter choice: ");
-            Scanner scanner = new Scanner(System.in);
-            int choice = scanner.nextInt();
-            scanner.nextLine();
 
-            switch (choice) {
-                case 1: {
-                    System.out.println("Enter your name: ");
-                    String name = scanner.nextLine();
-                    patient.setName(name);
 
-                    System.out.println("Enter your age: ");
-                    int age = scanner.nextInt();
-                    patient.setAge(age);
 
-                    System.out.println("Enter your height: ");
-                    double height = scanner.nextDouble();
-                    System.out.println("Enter your weight: ");
-                    double weight = scanner.nextDouble();
-                    patient.setDetails(height,weight);
 
-                    break;
-                }
-                case 2: {
-                    System.out.println(String.format("Name: %s Age: %d BMI %.2f",
-                            patient.getName(), patient.getAge(), patient.getBMI()));
+    running: while(true) {
 
-                    break;
-                }
-                case 3: {
-                    System.out.println("Good bye!");
-                    break running;
-                }
+
+        System.out.println("Please select from the " +
+                "following menu options:\n" +
+                "\t1. Add new patient\n" +
+                "\t2. View patient\n" +
+                "\t3. Exit\n");
+        System.out.print("Enter choice: ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1: {
+                addPatient(patient, scanner);
+
+                break;
+            }
+            case 2: {
+                viewPatient(patient);
+
+                break;
+            }
+            case 3: {
+                System.out.println("Good bye!");
+                break running;
             }
         }
+    }
+    }
+    private static void viewPatient(Patient patient) {
+        System.out.println(String.format("Name: %s Age: %d BMI %.2f",
+                patient.getName(), patient.getAge(), patient.getBMI()));
 
     }
+
+    private static void addPatient(Patient patient, Scanner scanner) {
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        patient.setName(name);
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        patient.setAge(age);
+
+        System.out.println("Enter your height: ");
+        double height = scanner.nextDouble();
+        System.out.println("Enter your weight: ");
+        double weight = scanner.nextDouble();
+        patient.setDetails(height, weight);
+    }
 }
+
